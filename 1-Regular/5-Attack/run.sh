42d41cf756d556ebc53b483541799f2ec4b07797
Gregory Plumb <gplumb@wisc.edu>
2018-09-27 16:21:31 -0400

    Update run.sh

--- 1-Regular/5-Attack/run.sh
+++ 1-Regular/5-Attack/run.sh
@@ -12,6 +12,3 @@ python run.py "CIFAR" "0.031" "classifier" "1" "51" "0.0"
 # Using PGD based attacks to define K-NN is not robust
 #python run.py "CIFAR" "0.031" "classifier" "3" "51" "0.0"
 #python run.py "CIFAR" "0.031" "classifier" "4" "51" "0.0"
-
-python ping.py
-
